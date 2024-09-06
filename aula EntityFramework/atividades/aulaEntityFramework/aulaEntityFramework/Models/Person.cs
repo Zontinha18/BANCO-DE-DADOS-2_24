461f993454cfb9cc1583717c6a4f8e35743b192a
Zontinha18 <143565209+Zontinha18@users.noreply.github.com>
2024-09-06 19:44:06 -0300

    Add files via upload

---
+++ aula EntityFramework/atividades/aulaEntityFramework/aulaEntityFramework/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace aulaEntityFramework.Models
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public List<Adress>? Adresses { get; set; }
+    }
+}
