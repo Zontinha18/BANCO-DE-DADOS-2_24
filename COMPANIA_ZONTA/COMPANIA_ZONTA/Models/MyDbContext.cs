b53ec02eacf2ee48bd1f785b1a9e38428820d377
Leonardo Meimberg Zonta <418898@unoesc.lan>
2024-11-01 22:05:17 -0300

    Aula 01/11

---
+++ COMPANIA_ZONTA/COMPANIA_ZONTA/Models/MyDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace COMPANIA_ZONTA.Models
+
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+    }
+}
